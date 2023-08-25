2f4e62156663dc8459c4c29e5a714cf7a8b2a5bc
Guilherme Trevisan <guilherme.trevisan@netshowme.me>
2023-08-25 14:04:15 -0300

    fix: changes

--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 
-echo "Hello world";
+include_once __DIR__ . '/../vendor/autoload.php';
+include_once __DIR__ . '/helper.php';
+
---
+++ public/template.php
@@ -0,0 +1,19 @@
+<html>
+<title>#title#</title>
+<head>
+	<style>
+        code {
+            font-family: Consolas,"courier new";
+            color: crimson;
+            background-color: #f1f1f1;
+            padding: 2px;
+            font-size: 105%;
+        }
+	</style>
+</head>
+<body>
+
+#body#
+
+</body>
+</html>
